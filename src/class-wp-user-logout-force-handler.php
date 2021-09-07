476d4106d359735f93f4ca5a2cdc7e8978abf0fa
Amirhossein Meydani <amirhwsin@outlook.com>
2021-09-07 15:06:36 +0430

    Fixed v2 code bug 1/2

--- src/class-wp-user-logout-force-handler.php
+++ src/class-wp-user-logout-force-handler.php
@@ -4,9 +4,9 @@ defined( 'ABSPATH' ) or die(); // Exit if called directly
 
 class WP_User_Logout_Force_Handler {
     
-	private $make_offline_rate = 1;
+		private $make_offline_rate = 1;
 
-	private $destroy_others_on_login = false;
+		private $destroy_others_on_login = false;
 
     public function __construct() {
 
@@ -16,9 +16,8 @@ class WP_User_Logout_Force_Handler {
         add_action( 'init', array( $this, 'update_online_users_status' ) );
         add_action( 'init', array( $this, 'update_last_login' ) );
 
-		 add_action( 'wp_login', array( $this, 'handle_new_login' ), 99, 2 );
-
-		 add_action( 'wp_authenticate_user', array( $this, 'login_lockdown' ), 100, 1);
+		add_action( 'wp_login', array( $this, 'handle_new_login' ), 99, 2 );
+		add_action( 'wp_authenticate_user', array( $this, 'login_lockdown' ), 100, 1);
 
         // Returning if user don't have caps or page is not users page
         if ( 'users.php' !== $pagenow || ! $this->user_has_cap() )
@@ -34,18 +33,16 @@ class WP_User_Logout_Force_Handler {
         add_filter( 'manage_users_custom_column', array($this, 'modify_ulf_status_row'), 10, 3 );
 
         add_filter( 'manage_users_sortable_columns', array( $this, 'make_ulf_field_sortable' ) );
-		 add_filter( 'users_list_table_query_args', array( $this, 'sort_by_login_activity' ) );
+		add_filter( 'users_list_table_query_args', array( $this, 'sort_by_login_activity' ) );
 
         add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 
-		 add_action( 'load-users.php', array( $this, 'handle_requests' ) );
-		 add_action( 'load-users.php', array( $this, 'handle_bulk_actions' ) );
-
-		 add_filter( 'bulk_actions-users', array( $this, 'add_bulk_action' ) );
-
-		 // Setting user options
-		 $this->make_offline_rate 		= get_option( 'ulf_make_offline', 1 );
-		 $this->destroy_others_on_login 	= get_option( 'ulf_destroy_others', false );
+		add_action( 'load-users.php', array( $this, 'handle_requests' ) );
+		add_action( 'load-users.php', array( $this, 'handle_bulk_actions' ) );
+		add_filter( 'bulk_actions-users', array( $this, 'add_bulk_action' ) );
+		// Setting user options
+		$this->make_offline_rate 		= get_option( 'ulf_make_offline', 1 );
+		$this->destroy_others_on_login 	= get_option( 'ulf_destroy_others', false );
     }
 
     public function include_ulf_method( $contact_methods ) {
@@ -78,11 +75,11 @@ class WP_User_Logout_Force_Handler {
 			 	$value .= ' </br>';
 			 	$value .= '<a style="color:red" href="' . esc_url( $logout_link ) . '">' . _x( 'Logout', 'The action on users list page', ULF_TEXT_DOMAIN ) . '</a>';
             } else {
-			 	$last_login = $this->get_last_login( $user_id );
-			 	$value      = '<span class="offline-circle">' . esc_html__( 'Offline ', ULF_TEXT_DOMAIN );
-			 	$value     .= '</br>' . esc_html__( 'Last Login: ', ULF_TEXT_DOMAIN );
-			 	$value     .= ! empty( $last_login ) ? $last_login . ' ago' : esc_html__( 'Never', ULF_TEXT_DOMAIN ) . '</span>';
-			 }
+				$last_login = $this->get_last_login( $user_id );
+				$value      = '<span class="offline-circle">' . esc_html__( 'Offline ', ULF_TEXT_DOMAIN );
+				$value     .= '</br>' . esc_html__( 'Last Login: ', ULF_TEXT_DOMAIN );
+				$value     .= ! empty( $last_login ) ? $last_login . ' ago' : esc_html__( 'Never', ULF_TEXT_DOMAIN ) . '</span>';
+			}
         }
 
         return $value;
@@ -99,20 +96,20 @@ class WP_User_Logout_Force_Handler {
         $logged_in_users = get_transient( 'online_status' );
         
         // Online, if (s)he is in the list and last activity was less than 60 seconds ago
-		 return isset( $logged_in_users[ $user_id ] ) && ( $logged_in_users[ $user_id ] > ( time() - ( $this->get_make_offline_time() * 60 ) ) );
+		return isset( $logged_in_users[ $user_id ] ) && ( $logged_in_users[ $user_id ] > ( time() - ( $this->get_make_offline_time() * 60 ) ) );
     }
 
     public function enqueue_scripts() {
         wp_enqueue_style( 'user-logout-force', plugins_url( 'dist/css/user-logout-force.css', WP_USER_LOGOUT_FORCE_PLUGIN_FILE ), array(), ULF_ABSPATH, $media = 'all' );
-		 wp_enqueue_script( 'user-logout-force-js', plugins_url( 'dist/js/script.js', WP_USER_LOGOUT_FORCE_PLUGIN_FILE ), array(), ULF_ABSPATH, false );
-		 wp_localize_script(
-		 	'user-logout-force-js',
-		 	'ulf_plugins_params',
-		 	array(
-		 		'ajax_url'     => admin_url( 'admin-ajax.php' ),
-		 		'review_nonce' => wp_create_nonce( 'review-notice' ),
-		 	)
-		 );
+		wp_enqueue_script( 'user-logout-force-js', plugins_url( 'dist/js/script.js', WP_USER_LOGOUT_FORCE_PLUGIN_FILE ), array(), ULF_ABSPATH, false );
+		wp_localize_script(
+			'user-logout-force-js',
+			'ulf_plugins_params',
+			array(
+				'ajax_url'     => admin_url( 'admin-ajax.php' ),
+				'review_nonce' => wp_create_nonce( 'review-notice' ),
+			)
+		);
     }
 
     /**
@@ -133,8 +130,8 @@ class WP_User_Logout_Force_Handler {
 		// Needs if user is not in the list.
 		$no_need_to_update = isset( $logged_in_users[ $user->ID ] )
 
-			 // And if his "last activity" was less than let's say ...6 seconds ago
-			 && $logged_in_users[ $user->ID ] > ( time() - ( 1 * 60 ) );
+			// And if his "last activity" was less than let's say ...6 seconds ago
+			&& $logged_in_users[ $user->ID ] > ( time() - ( 1 * 60 ) );
 
 		// Update the list if needed
         
@@ -176,66 +173,64 @@ class WP_User_Logout_Force_Handler {
 
 	private function logout_user( $user_id ) {
 
-		 $sessions = WP_Session_Tokens::get_instance( $user_id );
+		$sessions = WP_Session_Tokens::get_instance( $user_id );
 
-		 $sessions->destroy_all();
+		$sessions->destroy_all();
 
 	}
 
 	private function logout_all_users() {
-		 $users = get_users();
- 
-		 foreach ( $users as $user ) {
- 
-		 	// Get all sessions for user with ID $user_id
-		 	$sessions = WP_Session_Tokens::get_instance( $user->ID );
- 
-		 	// We have got the sessions, destroy them all!
-		 	$sessions->destroy_all();
-		 }
+		$users = get_users();
+
+		foreach ( $users as $user ) {
+
+			// Get all sessions for user with ID $user_id
+			$sessions = WP_Session_Tokens::get_instance( $user->ID );
+
+			// We have got the sessions, destroy them all!
+			$sessions->destroy_all();
+		}
 	}
 
 	public function handle_bulk_actions() {
+		if ( empty( $_REQUEST['users'] ) || empty( $_REQUEST['action']) )
+			return;
+			
+		if ( 'ulf-bulk-action' !== $_REQUEST['action'] )
+			return;
 		
-		 if ( empty( $_REQUEST['users'] ) || empty( $_REQUEST['action']) )
-		 	return;
-		 	
-		 if ( 'ulf-bulk-action' !== $_REQUEST['action'] )
-		 	return;
-		 
-		 $users = array_map( 'absint', (array) $_REQUEST['users'] );
-		 
-		 foreach ($users as $user) {
-		 	
-		 	 $this->logout_user( $user );
-		 }
- 
-		 return admin_url( 'users.php' );
+		$users = array_map( 'absint', (array) $_REQUEST['users'] );
+		
+		foreach ($users as $user) {
+			
+			 $this->logout_user( $user );
+		}
+
+		return admin_url( 'users.php' );
 	}
 
 	public function add_bulk_action( $actions ) {
-		 $actions['ulf-bulk-action'] = esc_html__( 'Logout', ULF_TEXT_DOMAIN );
-		
-		 return $actions;
+		$actions['ulf-bulk-action'] = esc_html__( 'Logout', ULF_TEXT_DOMAIN );
+	
+		return $actions;
 	}
 
 	public function sort_by_login_activity( $args ) {
-		
-		 if ( isset( $args['orderby'] ) && 'ulf_status' == $args['orderby'] ) {
- 
-		 	$order = isset( $args['order'] ) && $args['order'] === 'asc' ? 'desc' : 'asc';
- 
-		 	$args = array_merge(
-		 		$args,
-		 		array(
-		 			'meta_key' => 'last_login',
-		 			'orderby'  => 'meta_value',
-		 			'order'    => $order,
-		 		)
-		 	);
-		 }
- 
-		 return $args;
+		if ( isset( $args['orderby'] ) && 'ulf_status' == $args['orderby'] ) {
+
+			$order = isset( $args['order'] ) && $args['order'] === 'asc' ? 'desc' : 'asc';
+
+			$args = array_merge(
+				$args,
+				array(
+					'meta_key' => 'last_login',
+					'orderby'  => 'meta_value',
+					'order'    => $order,
+				)
+			);
+		}
+
+		return $args;
 	}
 
     /**
@@ -246,79 +241,77 @@ class WP_User_Logout_Force_Handler {
 	 * @return void.
 	 */
 	public function update_last_login() {
-
-		 $user_id = get_current_user_id();
-		 update_user_meta( $user_id, 'last_login', time() );
+		$user_id = get_current_user_id();
+		update_user_meta( $user_id, 'last_login', time() );
 	}
 
     public function get_last_login( $user_id ) {
-         $last_login     = get_user_meta( $user_id, 'last_login', true );
-		 $the_login_date = '';
- 
-		 if ( ! empty( $last_login ) && 0.00058373 != $last_login ) {
-		 	
-		 	$the_login_date = human_time_diff( $last_login );
-		 }
- 
-		 return $the_login_date;
+        $last_login     = get_user_meta( $user_id, 'last_login', true );
+		$the_login_date = '';
+
+		if ( ! empty( $last_login ) && 0.00058373 != $last_login ) {
+			$the_login_date = human_time_diff( $last_login );
+		}
+
+		return $the_login_date;
     }
 
 	public function handle_new_login( $user, $user_id ) {
 		
-		 if ( $this->destroy_others_on_login() ) {
-		 	$sessions 	= WP_Session_Tokens::get_instance( $user_id );
-		 	$token		= wp_get_session_token();
-	 
-		 	$sessions->destroy_others( $token );
-		 }
+		if ( $this->destroy_others_on_login() ) {
+			$sessions 	= WP_Session_Tokens::get_instance( $user_id );
+			$token		= wp_get_session_token();
+	
+			$sessions->destroy_others( $token );
+		}
 
 	}
 
 	public function login_lockdown( $user ) {
 
-		 if ( ! $this->is_login_lockdown_active() )
-		 	return $user;
- 
-		 if ( is_wp_error( $user ) ) {
-		 	return $user;
-		 }
- 
-		 // Is user role is in white list or not
-		 $failure = $this->lockdown_whitelist_check( $user->ID );
-		 
-		 if ( ! $failure ) {
-		 	return new WP_Error( 'ulf_login_lockdown', __( "Sorry, Login is disabled right now. Try again in few minutes.", ULF_TEXT_DOMAIN ) );
-		 }
-	 
-		 return $user;
+		if ( ! $this->is_login_lockdown_active() )
+			return $user;
+
+		if ( is_wp_error( $user ) ) {
+			return $user;
+		}
+
+		// Is user role is in white list or not
+		$failure = $this->lockdown_whitelist_check( $user->ID );
+		
+		if ( ! $failure ) {
+			return new WP_Error( 'ulf_login_lockdown', __( "Sorry, Login is disabled right now. Try again in few minutes.", ULF_TEXT_DOMAIN ) );
+		}
+	
+		return $user;
 	}
 
 	private function is_login_lockdown_active() {
-		 return (get_option( 'ulf_lock_login', 'no' ) === 'on') ? true : false;
+		return (get_option( 'ulf_lock_login', 'no' ) === 'on') ? true : false;
 	}
 
 	private function lockdown_whitelist_check( $user_id ) {
-		 $user_meta	=	get_userdata($user_id);
-		 $user_role	=	$user_meta->roles;
- 
-		 $white_role = get_option( 'ulf_login_while_list', false);
-		 
-		 if ( false === $white_role )
-		 	return true;
- 
-		 foreach ($user_role as $role)
-		 	if ( $white_role === $role)
-		 		return true;
-		 
-		 return false;
+		$user_meta	=	get_userdata($user_id);
+		$user_role	=	$user_meta->roles;
+
+		$white_role = get_option( 'ulf_login_while_list', false);
+		
+		if ( false === $white_role )
+			return true;
+
+		foreach ($user_role as $role)
+			if ( $white_role === $role)
+				return true;
+		
+		return false;
 	}
 	
 	private function get_make_offline_time() {
-		 return $this->make_offline_rate;
+		return $this->make_offline_rate;
 	}
 
 	private function destroy_others_on_login() {
-		 return $this->destroy_others_on_login;
+		return $this->destroy_others_on_login;
 	}
 
     /**
@@ -328,15 +321,14 @@ class WP_User_Logout_Force_Handler {
      * @return  bool
      */
     private function user_has_cap() {
+        if ( is_multisite() ) {
+            $blog_id    = get_current_blog_id();
+            $has_cap    = current_user_can_for_blog( $blog_id, 'edit_users' );
+        }else {
+            $has_cap    = current_user_can( 'edit_users' );
+        }
 
-         if ( is_multisite() ) {
-             $blog_id    = get_current_blog_id();
-             $has_cap    = current_user_can_for_blog( $blog_id, 'edit_users' );
-         }else {
-             $has_cap    = current_user_can( 'edit_users' );
-         }
- 
-         return $has_cap;
+        return $has_cap;
     }
 }
 
